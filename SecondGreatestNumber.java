a03cfa2c94fa4688f9ca61cf44a8bb58fe7c8851
isrchary02 <isrchary02@gmail.com>
2024-05-23 20:06:11 +0530

    Update SecondGreatestNumber.java

--- SecondGreatestNumber.java
+++ SecondGreatestNumber.java
@@ -6,9 +6,22 @@ public class SecondGreatestNumber {
         int size = sc.nextInt();
         int [] array = new int[size];
 
+        System.out.println("Enter the elements in to the Array ")
         for (int i=0; i<size; i++){
             array[i] = sc.nextInt();
         }
         
+        int firstLargest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (num > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = num;
+            } else if (num > secondLargest && num < firstLargest) {
+                secondLargest = num;
+            }
+        }
+        System.out.println("The Second Largest Number is: " + secondLargest);
     }
 }
